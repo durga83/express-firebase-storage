ac6c395effb826a1ea36d7c4aed484a4ebec01fa
Durga <durgaltbk@gmail.com>
2019-09-17 12:36:07 +0530

    minor changes

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const fireBaseRoute = require('./app/route/firebase.route');
 const config = require('./app/config/config');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use('/firebase', fireBaseRoute);
+
 app.listen(config.port, () => console.log(`app listening on port ${config.port}!`));
